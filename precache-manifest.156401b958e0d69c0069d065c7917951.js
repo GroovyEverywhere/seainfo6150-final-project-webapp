d078ca058c746f87826ef588be8ae67d18e99da0
GroovyEverywhere <leitanbruno@gmail.com>
2020-12-02 18:10:47 +0800

    Updates

---
+++ precache-manifest.156401b958e0d69c0069d065c7917951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4d65a74b6dd0dc7a2b2bbbecda693a",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "c77e15add5f23913b89b",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.181dc0bc.chunk.css"
+  },
+  {
+    "revision": "45b69d9c4accf42e7751",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.4b607719.chunk.js"
+  },
+  {
+    "revision": "c77e15add5f23913b89b",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.6edee943.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "46ed06d37eac69d3bc10ea29de325e32",
+    "url": "/seainfo6150-final-project-webapp/static/media/2.46ed06d3.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seainfo6150-final-project-webapp/precache-manifest.316b9f3f246cd509f0e161edb5c986f7.js"
+  "/seainfo6150-final-project-webapp/precache-manifest.156401b958e0d69c0069d065c7917951.js"
 );
 
 self.addEventListener('message', (event) => {
