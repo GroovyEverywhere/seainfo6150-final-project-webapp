a4ad6e67915a8aa5e4c375f5b7e84b78025f3f3a
GroovyEverywhere <leitanbruno@gmail.com>
2020-12-02 18:15:32 +0800

    “[final]”

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 
-import { Switch, Route, Link } from "react-router-dom";
-
-import React, { useEffect, useState } from "react";
-
-import Homepage from "./final/homepage/homepage.jsx";
-import { isEmpty } from "lodash";
-// import "./App.css"
-import Categories from "./final/categories/categories.jsx"
-
-function App() {
 
 
   return  (
     <div className="App">
-     
-      <Switch>
-        <Route exact path="/homepage"><Homepage/></Route>
-        <Route exact path="/categories"><Categories/></Route>
-      </Switch>
+     哈哈哈哈哈
     </div>
   );
-}
+
 
 
 export default App;
