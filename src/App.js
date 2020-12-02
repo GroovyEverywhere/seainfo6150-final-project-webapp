c202cd29032d35f286517958e8534ddcbbdd04a4
GroovyEverywhere <leitanbruno@gmail.com>
2020-12-02 18:32:23 +0800

    “[final]”

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react"
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./final/homepage/homepage.jsx";
+import Categories from "./final/categories/categories.jsx"
 
 function App() {
   return  (
-    <div >
-     哈哈哈哈哈
+    <div className="App">
+     <Switch>
+        <Route exact path="/homepage"><Homepage/></Route>
+        <Route exact path="/categories"><Categories/></Route>
+      </Switch>
     </div>
   );
 
