84edff054461201fe530cf5b406dc65fa8b0e48e
GroovyEverywhere <leitanbruno@gmail.com>
2020-12-02 14:19:08 +0800

    Updates

---
+++ precache-manifest.ad87f215504efaf8001b04c7d3ae3d7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c9c00fb29b7bee65b8cae512817745",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "f5ecd6e4a933692aaf1e",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.e285c893.chunk.css"
+  },
+  {
+    "revision": "a583836c99ccf1da802f",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.8179a6a5.chunk.js"
+  },
+  {
+    "revision": "f5ecd6e4a933692aaf1e",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.7bcc6217.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "46ed06d37eac69d3bc10ea29de325e32",
+    "url": "/seainfo6150-final-project-webapp/static/media/2.46ed06d3.PNG"
+  }
+]);
